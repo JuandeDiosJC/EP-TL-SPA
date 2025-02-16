9e20e2b92a9e659e5f64eca74816aca558c9f487
Juan de Dios Juárez Castro <juarez.castro.juan1cv1@gmail.com>
2025-02-16 01:44:34 -0600

    Diseno Inicio

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,18 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen grid grid-rows-[auto_1fr_auto] gap-8 p-4 sm:p-8 md:p-16 font-[family-name:var(--font-geist-sans)]">
-      {/* Header (opcional) */}
+    <div className="min-h-screen grid grid-rows-[auto_1fr_auto] gap-8 p-4 sm:p-8 md:p-16 bg-gradient-to-r from-[#f5f5dc] to-[#faf7f0] font-[family-name:var(--font-geist-sans)]">
+      {/* Header */}
       <header className="w-full text-center">
         <h1 className="text-3xl font-bold">Bienvenid@ a Todo List</h1>
       </header>
 
       <main className="flex flex-col items-center justify-center gap-8">
         <div className="w-full max-w-xl flex flex-col gap-8 items-center text-center">
+          {/* Imagen centrada */}
           <Image
-            className="dark:invert mx-auto"
+            className="mx-auto"
             src="/lmagelog.png"
-            alt="Next.js logo"
+            alt="Logo"
             width={180}
             height={40}
             priority
@@ -21,7 +22,7 @@ export default function Home() {
           <ol className="list-inside list-decimal text-sm font-[family-name:var(--font-geist-mono)]">
             <li className="mb-2">
               Cada tarea completada,{" "}
-              <code className="bg-black/5 dark:bg-white/6 px-1 py-0.5 rounded font-semibold">
+              <code className="bg-black/5 px-1 py-0.5 rounded font-semibold">
                 es un paso hacia el éxito.
               </code>
             </li>
@@ -32,13 +33,13 @@ export default function Home() {
 
           <div className="flex flex-col sm:flex-row items-center justify-center gap-4">
             <a
-              className="rounded-full border transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+              className="rounded-full border transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
               href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
             >
               <Image
-                className="dark:invert"
+                className="invert"
                 src="/vercel.svg"
                 alt="Vercel logomark"
                 width={20}
@@ -47,7 +48,7 @@ export default function Home() {
               Registrate
             </a>
             <a
-              className="rounded-full border border-black/8 dark:border-white/15 transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-[180px]"
+              className="rounded-full border transition-colors flex items-center justify-center hover:bg-gray-200 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-[180px]"
               href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
